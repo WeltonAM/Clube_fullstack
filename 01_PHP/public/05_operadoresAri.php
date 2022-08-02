611aadc7921997c847e41fbcc9e2ba278b787775
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-02 14:16:22 -0300

    Update

--- 01_PHP/public/05_operadoresAri.php
+++ 01_PHP/public/05_operadoresAri.php
@@ -3,5 +3,5 @@
 $num1 = 10;
 $num2 = 20;
 
-$opera = ($num1 + $num2) * ($num1 / $num2) - 2;
+$opera = ($num1 + $num2) * ($num1 / $num2) - 1 % 2;
 
---
+++ 01_PHP/public/06_opAtribui.php
@@ -0,0 +1,13 @@
+<?php 
+
+$name = 'Juliana';
+$name .= ' Karla';
+
+echo $name;
+
+
+$num = 50;
+$num *= 2;
+
+echo $num;
+
