577cd02a5650a8a108119e837258b1d9965fac57
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-02 13:29:15 -0300

    class 01

---
+++ 01_PHP/public/01_index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Maneiras de importar outro arquivo
+include './teste.php';
+
+// include_once './teste.php'; 
+// _ once -> inlcuir uma única vez
+
+// require './teste.php';
+// require -> não continua a execução do programa
+
+
+
+echo $person;
