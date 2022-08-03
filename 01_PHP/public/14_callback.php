c90e277b85a9720e6489018d0c955a6a9889be74
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-03 15:16:04 -0300

    Create 14_callback.php

---
+++ 01_PHP/public/14_callback.php
@@ -0,0 +1,56 @@
+<?php 
+
+// function teste($name)
+// {
+//     return "Olá, meu nome é {$name}";
+// }
+
+// function teste2($fn)
+// {
+    // if(is_callable($fn)){
+    // return $fn('Juliana');
+//    }
+// }
+
+// echo teste2('teste');
+
+// ----------------------
+// function teste($name)
+// {
+//     return "Olá, {$name}";
+// }
+
+// echo call_user_func('teste', 'Karla'); // uma callback, e sua maneira de passar parâmetro
+
+
+// ----------------------
+// class User 
+// {
+//     public function teste($name)
+//     {
+//         return "Olá, {$name}";
+//     }
+// }
+
+// $user = new User; // caso esteja em uma função STATIC não precisa instanciar nova classe
+
+// echo call_user_func([$user, 'teste'], 'Flor');
+
+
+// ----------------------
+class User
+{
+    public function __invoke()
+    {
+        return 'Aqui o teste';
+    }
+}
+
+$user = new User;
+
+function teste($fn)
+{
+    return $fn();
+}
+
+echo teste($user);
