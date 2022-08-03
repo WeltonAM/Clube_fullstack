875e87878986f4484757817cccc6ea886fafb855
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-03 17:43:30 -0300

    Post

--- 01_PHP/public/15_superGlobais/index.php
+++ 01_PHP/public/15_superGlobais/index.php
@@ -1,10 +1,29 @@
 <?php 
 
-
 // setcookie('name', 'Juliana', time() + 2 * 24 * 60 * 60);
 
-session_start();
+// session_start();
 
-require '02_session.php';
+// require '02_session.php';
 
-$_SESSION['name'] = 'Karla';
+// $_SESSION['name'] = 'Karla';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+
+    <form action="03_post.php">
+        <input type="text" name="name" placeholder="Nome">
+        <input type="email" name="email" placeholder="Email">
+        <button type="submit">Cadastrar</button>
+    </form>
+
+</body>
+</html>
