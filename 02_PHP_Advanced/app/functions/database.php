04e97aedc94daf866c058185b0c79b7473ab125c
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-09 14:40:34 -0300

    Update DB

--- 02_PHP_Advanced/app/functions/database.php
+++ 02_PHP_Advanced/app/functions/database.php
@@ -1,71 +1,94 @@
 <?php
 
-function connect(){
-    $pdo = new \PDO("mysql:host=localhost;dbname=blog;charset=utf8", 'root', '12Nas89.');
+function connect() {
+	$pdo = new \PDO("mysql:host=localhost;dbname=blog;charset=utf8", 'root', '12Nas89.');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+	return $pdo;
 }
 
-function create($table, $fields){
-    
-    if(!is_array($fields)){
-        $fields = (array) $fields;
-    }
-    
-    $sql = "insert into {$table}";
-    $sql .= "(" . implode(',', array_keys($fields)) . ")";
-    $sql .= " values(" . ":" . implode(',:', array_keys($fields)) . ")";
-    
-    $pdo = connect();
-    $insert = $pdo->prepare($sql);
+function create($table, $fields) {
 
-    return $insert->execute($fields);
-}
+	if (!is_array($fields)) {
+		$fields = (array) $fields;
+	}
 
-function all($table){
-    $pdo = connect();
+	$sql = "insert into {$table}";
+	$sql .= "(" . implode(',', array_keys($fields)) . ")";
+	$sql .= " values(" . ":" . implode(',:', array_keys($fields)) . ")";
 
-    $sql = "select * from {$table}";
-    $list = $pdo->query($sql);
+	$pdo = connect();
 
-    $list->execute();
+	$insert = $pdo->prepare($sql);
 
-    return $list->fetchAll();
-}
-
-function update($table, $fields, $where){
-    if(!is_array($fields)){
-        $fields = (array) $fields;
-    }
-
-    $pdo = connect();
-
-    $fileds = array_map(function($field){
-        return "{$field} = :{$field}";
-    }, array_key($fileds));
-
-    $sql = "update {$table} set";
-
-    $sql .= implode(',', $fields);
-
+	return $insert->execute($fields);
 
 }
 
-function find($table, $field, $value){
-    $pdo = connect();
+function all($table) {
 
-    $value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+	$pdo = connect();
 
-    $sql = "select * from {$table} where {$field} = :{$field}";
+	$sql = "select * from {$table}";
+	$list = $pdo->query($sql);
 
-    $find = $pdo->prepare($sql);
-    $find->bindValue($field, $value);
-    $find->execute();
+	$list->execute();
 
-    return $find->fetch();
+	return $list->fetchAll();
+
 }
 
-function delete(){
+function update($table, $fields, $where) {
 
+	if (!is_array($fields)) {
+		$fields = (array) $fields;
+	}
+
+	$data = array_map(function ($field) {
+		return "{$field} = :{$field}";
+	}, array_keys($fields));
+
+	$sql = "update {$table} set ";
+
+	$sql .= implode(',', $data);
+
+	$sql .= " where {$where[0]} = :{$where[0]}";
+
+	$data = array_merge($fields, [$where[0] => $where[1]]);
+
+	dd($data);
+
+	$pdo = connect();
+
+	$update = $pdo->prepare($sql);
+
+	$update->execute($data);
+
+	return $update->rowCount();
+
+}
+
+function find($table, $field, $value) {
+	$pdo = connect();
+
+	$value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+
+	$sql = "select * from {$table} where {$field} = :{$field}";
+
+	$find = $pdo->prepare($sql);
+	$find->bindValue($field, $value);
+	$find->execute();
+
+	return $find->fetch();
+}
+
+function delete($table, $field, $value) {
+	$pdo = connect();
+
+	$sql = "delete from {$table} where {$field} = :{$field}";
+	$delete = $pdo->prepare($sql);
+	$delete->bindValue($field, $value);
+
+	return $delete->execute();
 }
--- 02_PHP_Advanced/public/pages/home.php
+++ 02_PHP_Advanced/public/pages/home.php
@@ -2,32 +2,35 @@
 
 <a href="?page=create_user">Cadastrar usuário</a>
 
+<?=get('message');?>
 <table class="table table-striped table-hover">
     <thead>
         <tr>
             <th>Id</th>
-            <th>Name</th>
+            <th>Nome</th>
             <th>Email</th>
             <th></th>
             <th></th>
         </tr>
     </thead>
     <tbody>
-        <?php 
+        <?php
             $users = all('users');
-            foreach ($users as $user);
+            foreach ($users as $user):
         ?>
 
         <tr>
-            <td><?= $user->id; ?></td>
-            <td><?= $user->name; ?></td>
-            <td><?= $user->email; ?></td>
+            <td><?=$user->id;?></td>
+            <td><?=$user->name;?></td>
+            <td><?=$user->email;?></td>
             <td>
-                <a href="?page=edit_user&id=<?= $user->id; ?>" class="btn btn-success">Editar</a>
+                <a href="?page=edit_user&id=<?=$user->id;?>" class="btn btn-success">Editar</a>
             </td>
             <td>
-                <a href="" class="btn btn-danger">Deletar</a>
+                <a href="?page=delete_user&id=<?=$user->id;?>" class="btn btn-danger">Deletar</a>
             </td>
         </tr>
+
+        <?php endforeach;?>
     </tbody>
 </table>
