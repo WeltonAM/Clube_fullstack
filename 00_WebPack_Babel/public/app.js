0467338327dd594a0ca338cbd8a2713eb26e1a61
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-23 15:44:11 -0300

    Webpack and Babel

---
+++ 00_WebPack_Babel/public/app.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+var _element$index;
+
+(_element$index = element.index) !== null && _element$index !== void 0 ? _element$index : -1;
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
