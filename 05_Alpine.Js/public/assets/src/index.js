2c129fefa835cc6fd77290e11bb51c32b0ebbe5c
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-08-25 17:03:30 -0300

    Template list data

--- 05_Alpine.Js/public/assets/src/index.js
+++ 05_Alpine.Js/public/assets/src/index.js
@@ -1,5 +1,30 @@
 function app(){
     return{
-        show: true
+        show: true,
+        users:[],
+        data(){
+            this.users = [
+                {
+                    clienteID: 1,
+                    nomecompleto: 'Juliana',
+                    cidade: 'FSA'
+                },
+                {
+                    clienteID: 2,
+                    nomecompleto: 'Karla',
+                    cidade: "Welton's heart"
+                },
+                {
+                    clienteID: 3,
+                    nomecompleto: 'Frô',
+                    cidade: 'Serra'
+                },
+                {
+                    clienteID: 4,
+                    nomecompleto: 'Flormosura',
+                    cidade: "God's garden"
+                },
+            ]
+        }
     }
 }
