45e8ef7db81a7f76c10604d58624d61a79c251e9
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-09-20 21:16:16 -0300

    Composer utilities

---
+++ 08_Composer/app/classes/Email.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Email extends PHPMailer
+{
+
+    public function send()
+    {
+        return 'enviar email';
+    }
+}
---
+++ 08_Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pilgr/08_composer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a718323118b6ea420c9c11f91427a41be6b1fcd7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.4',
+            'version' => '6.6.4.0',
+            'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pilgr/08_composer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a718323118b6ea420c9c11f91427a41be6b1fcd7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
