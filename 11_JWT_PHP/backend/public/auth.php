ef478f3e9e38210c3f316778831f0cf66a7117fd
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-09-22 18:10:05 -0300

    Auth the token

--- 11_JWT_PHP/backend/public/auth.php
+++ 11_JWT_PHP/backend/public/auth.php
@@ -1,2 +1,24 @@
 <?php
 
+require '../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use Firebase\JWT\JWT;
+
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
+
+$dotenv = Dotenv::createImmutable(dirname(__FILE__, 2));
+$dotenv->load();
+
+$authorization = $_SERVER["HTTP_AUTHORIZATION"];
+
+$token = str_replace('Bearer ', '', $authorization);
+
+try {
+    $decoded = JWT::decode($token, $_SERVER['KEY'], ['HS256']);
+} catch (Throwable $e) {
+    if($e->getMessage() === 'Expired token'){
+        http_response_code(401);
+        die($e->getMessage());
+    }
+}
