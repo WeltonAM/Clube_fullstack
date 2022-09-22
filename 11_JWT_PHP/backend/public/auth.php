b925551158137cb0d881ff1c5546d85895e024bd
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-09-22 18:19:49 -0300

    Debugging

--- 11_JWT_PHP/backend/public/auth.php
+++ 11_JWT_PHP/backend/public/auth.php
@@ -4,6 +4,7 @@ require '../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 
@@ -15,7 +16,7 @@ $authorization = $_SERVER["HTTP_AUTHORIZATION"];
 $token = str_replace('Bearer ', '', $authorization);
 
 try {
-    $decoded = JWT::decode($token, $_SERVER['KEY'], ['HS256']);
+    $decoded = JWT::decode($token, new Key($_SERVER['KEY'], 'HS256'));
 } catch (Throwable $e) {
     if($e->getMessage() === 'Expired token'){
         http_response_code(401);
