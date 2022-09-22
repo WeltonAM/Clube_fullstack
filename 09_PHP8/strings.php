0c4bddd71aaaa48f1dfd9ff9fb2b689125eb0d5c
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-09-22 07:39:28 -0300

    String functions

---
+++ 09_PHP8/strings.php
@@ -0,0 +1,7 @@
+<?php
+
+$string = 'My is Juliana';
+
+var_dump(str_contains($string, 'Juliana')).PHP_EOL;
+var_dump(str_starts_with($string, 'm')).PHP_EOL;
+var_dump(str_ends_with($string, 'a')).PHP_EOL;
