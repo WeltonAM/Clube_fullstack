7240409c788859e4c6f4092c2df4dbf479337df8
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-09-23 09:26:08 -0300

    Add item to the cart

--- 12_Shoppping_Cart/public/add.php
+++ 12_Shoppping_Cart/public/add.php
@@ -1,7 +1,13 @@
 <?php
 
+session_start();
+
 require '../vendor/autoload.php';
 
-$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+use app\classes\Cart;
 
-echo $id;
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$cart = new Cart;
+$cart->add($id);
+
+$cart->dump();
