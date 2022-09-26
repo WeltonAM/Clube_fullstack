2c149628cdc313714074d76325f31e1c00930907
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-09-26 17:04:12 -0300

    Exception controller verification

--- 13_MVC/core/Controller.php
+++ 13_MVC/core/Controller.php
@@ -3,10 +3,72 @@
 namespace core;
 
 use app\classes\Uri;
+use app\exceptions\ControllerNotExistException;
 
 class Controller
 {
-    public function __construct(private $uri)
+    private $controller;
+    private $namespace;
+
+    private $folders = [
+        'app\controllers\portal',
+        'app\controllers\admin',
+    ];
+
+    private $uri;
+
+    public function __construct()
     {
+        $this->uri = Uri::uri();
+    }
+
+    public function load()
+    {
+        if($this->isHome()){
+            return $this->controllerHome();
+        }
+
+        return $this->controllerNotHome();
+    }
+
+    private function controllerHome()
+    {
+        if(!$this->controllerExist('HomeController')){
+            throw new ControllerNotExistException("This page doesn't exist");
+        }
+
+        return $this->instanciateController();
+    }
+
+    private function controllerNotHome()
+    {
+
+    }
+
+    private function isHome()
+    {
+        return ($this->uri == '/');
+    }
+
+    private function controllerExist($controller)
+    {
+        $controllerExist = false;
+
+        foreach($this->folders as $folder){
+            if(class_exists($folder.'\\'.$controller)){
+                $controllerExist = true;
+                $this->namespace = $folder;
+                $this->controller = $controller;
+            }
+        }
+
+        return $controllerExist;
+    }
+
+    private function instanciateController()
+    {
+        $controller = $this->namespace.'\\'.$this->controller;
+        
+        return new $controller;
     }
 }
--- 13_MVC/public/index.php
+++ 13_MVC/public/index.php
@@ -1,3 +1,14 @@
 <?php
 
 require '../bootstrap.php';
+
+use app\classes\Uri;
+use core\Controller;
+
+try {
+    $controller = new Controller;
+    $controller = $controller->load();
+    dd($controller);
+} catch (\Exception $e) {
+    dd($e->getMessage());
+}
