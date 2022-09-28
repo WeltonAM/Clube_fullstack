ccf3b8a130aafc46b94f46a8d2c3b77fc15d3648
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-09-28 11:17:37 -0300

    Getting attributes

--- 14_Active_Record/public/index.php
+++ 14_Active_Record/public/index.php
@@ -1,2 +1,12 @@
 <?php
 
+require '../vendor/autoload.php';
+
+use app\database\models\User;
+
+$user = new User;
+$user->firstName = 'Juliana';
+$user->lastName = 'Karla';
+$user->id = 1;
+
+var_dump($user);
