0bfc4c12aea8591dbbe0f9d36ca86a8966b948fc
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-10-03 10:12:32 -0300

    The bootstrap

---
+++ 15_MVC_advanced/app/core/MyApp.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\core;
+
+use app\interfaces\ControllerInterface;
+
+class MyApp
+{
+    private $controller;
+
+    public function __construct(private ControllerInterface $controllerInterface)
+    {
+    }
+    
+    public function controller()
+    {
+        $controller = $this->controllerInterface->controller();
+        $method = $this->controllerInterface->method();
+        $params = $this->controllerInterface->params();
+
+        $this->controller = new $controller;
+        $this->controller->$method($params);
+    }
+    
+    public function view()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'GET'){
+            if(!isset($this->controller->data)){
+                throw new \Exception("Data is required");
+                
+            }
+        
+            if(!array_key_exists('title', $this->controller->data)){
+                throw new \Exception("Title is required");
+                
+            }
+        
+            extract($this->controller->data);
+            require '../app/views/index.php';
+        }
+    }
+}
--- 15_MVC_advanced/app/views/index.php
+++ 15_MVC_advanced/app/views/index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $controller->data['title']; ?></title>
+    <title><?php echo $this->controller->data['title']; ?></title>
 </head>
 <body>
     <h2>MVC Course</h2>
 
     <div>
-        <?php require VIEW_PATH.$controller->view; ?>
+        <?php require VIEW_PATH.$this->controller->view; ?>
     </div>
 </body>
 </html>
