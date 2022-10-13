7324e33bcaa51917c4748620cde9069a2be9068f
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-10-13 17:40:56 -0300

    Basic structure

---
+++ 16_PDO/public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+use app\classes\Uri;
+use app\classes\Routes;
+
+require "../bootstrap.php";
+
+$routes = [
+    '/' => 'controllers/index.php',
+];
+
+$uri = Uri::load();
+
+require Routes::load($routes, $uri);
