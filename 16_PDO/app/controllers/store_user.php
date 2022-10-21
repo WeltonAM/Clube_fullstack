b528509137f33ea68ab65a9c419e28bd4e900e49
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-10-20 21:46:36 -0300

    Transaction #1

--- 16_PDO/app/controllers/store_user.php
+++ 16_PDO/app/controllers/store_user.php
@@ -1,14 +1,27 @@
 <?php
 
-use app\classes\Validation;
 use app\models\User;
+use app\classes\Validation;
+use app\models\Transaction;
 
 $validation = new Validation;
 $validate = $validation->validate($_POST);
 
-$user = new User;
-$user->insert($validate);
+$transaction = new Transaction;
 
-if($user){
-    header('location:/');
-}
+$transaction->transactions(function() use($transaction, $validate){
+    $transaction->user->insert($validate);
+    
+    $transaction->post->insert([
+        'title' => 'teste',
+        'user' => 78,
+        'descriprion' => 'description teste'
+    ]);
+});
+
+// $user = new User;
+// $user->insert($validate);
+
+// if($user){
+//     header('location:/');
+// }
