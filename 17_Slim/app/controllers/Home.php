8d3590cc6b8b668644164568238a7b8b6d6c2e65
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-11-08 16:32:31 -0300

    Flash messages

--- 17_Slim/app/controllers/Home.php
+++ 17_Slim/app/controllers/Home.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\classes\Flash;
 use app\database\models\User;
 
 class Home extends Base
@@ -16,19 +17,14 @@ class Home extends Base
     public function index($request, $response)
     {
         $users = $this->user->find();
-    
-        // $created = $this->user->create(['firstName' => 'asas', 'email' => 'asasas']);
 
-        // $updated = $this->user->update(['fields' => ['firstName' => 'Juliana', 'email' => 'Juju'], 'where' => ['id' => 1]]);
-
-        $deleted = $this->user->delete('id', 2);
-
-        var_dump($deleted);
-        die();
+        Flash::set('message', 'Success to sign up');
+        $message = Flash::get('message');
 
         return $this->getTwig()->render($response, $this->setView('site/home'), [
             'title' => 'Home',
             'users' => $users,
+            'message' => $message,
         ]);
     }
 }
