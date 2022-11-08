da459d2d5adad519814803537b2c359c4cc8a3e8
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-11-08 17:51:36 -0300

    Overirde http methods

--- 17_Slim/app/controllers/User.php
+++ 17_Slim/app/controllers/User.php
@@ -4,12 +4,30 @@ namespace app\controllers;
 
 use app\classes\Flash;
 
-class User
+class User extends Base
 {
     public function create($request, $response, $args)
     {
-        Flash::set('message', 'teste');
+        return $this->getTwig()->render($response, $this->setView('site/user_create'), [
+            'title' => 'Sign up',
+        ]);
+    }
 
-        return redirect('/', $response);
+    public function store($request, $response, $args)
+    {
+        var_dump('sign up');
+        return $response;
+    }
+
+    public function update($request, $response, $args)
+    {
+        var_dump('update');
+        return $response;
+    }
+
+    public function destroy($request, $response, $args)
+    {
+        var_dump('delete');
+        return $response;
     }
 }
--- 17_Slim/public/index.php
+++ 17_Slim/public/index.php
@@ -5,11 +5,16 @@ session_start();
 require '../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
 
 $app = AppFactory::create();
 
 require '../app/routes/site.php';
 require '../app/routes/user.php';
+
+$methodOverrideMiddleware = new MethodOverrideMiddleware();
+$app->add($methodOverrideMiddleware);
+
 
 $app->run();
 
