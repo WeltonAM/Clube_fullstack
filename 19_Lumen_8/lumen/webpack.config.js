214b4938e036becfaf6a700dfc7ed44e05ba49f0
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-01 14:13:47 -0300

    Debugging

--- 19_Lumen_8/lumen/webpack.config.js
+++ 19_Lumen_8/lumen/webpack.config.js
@@ -4,24 +4,24 @@ const publicPath = path.resolve(__dirname, "public");
 module.exports = {
     mode:process.env.NODE_ENV,
     entry:{
-        login: publicPath + "assets/login.js",
+        login: publicPath + "/assets/login.js",
     },
     output:{
         path: publicPath + "/dist",
         filename: "[name].js"
     },
     module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ['@babel/preset-env']
-              }
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
             }
           }
-        ]
+        }
+      ]
     }
 }
