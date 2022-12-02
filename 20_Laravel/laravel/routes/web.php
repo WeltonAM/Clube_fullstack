de7b1e7084bcde6992fb06c485719bb46a08d7c8
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-12-02 08:43:10 -0300

    Working with routes

--- 20_Laravel/laravel/routes/web.php
+++ 20_Laravel/laravel/routes/web.php
@@ -1,8 +1,26 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// working with routes
+
+// Route::prefix('blog')->group(function(){
+//     Route::get('/', function(){
+//         dd('blog');
+//     });
+
+//     Route::get('/post/{slug}', function($slug){
+//         dd($slug);
+//     });
+// });
+
+// Route::get('/user/{name}/{age}', function ($user, $age) {
+//     return view('welcome');
+// })->where(['name' => '[a-z]+'. 'age' => '[0-9]+']);
+// ->whereAlpha('name')->whereNumber('age');
+
+// Route::get('/', [HomeController::class, 'index']);
