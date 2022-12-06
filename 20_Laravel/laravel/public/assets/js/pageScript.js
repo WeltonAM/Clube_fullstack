876989bcc86d3c9fb49ef36900b5c29cd412f933
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-06 18:36:25 -0300

    Debugging

--- 20_Laravel/laravel/public/assets/js/pageScript.js
+++ 20_Laravel/laravel/public/assets/js/pageScript.js
@@ -2,8 +2,10 @@ const spinnerWrapperEl = document.querySelector('.spinner-wrapper');
 
 window.addEventListener('DOMContentLoaded', () => {
     spinnerWrapperEl.style.opacity = '0';
+
+    const loader = window.performance.now();
     
     setTimeout(() => {
         spinnerWrapperEl.style.display = 'none';
-    }, 600);
+    }, loader);
 });
