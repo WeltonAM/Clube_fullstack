a3cf32f30ad791613ceec0a12ddd0bafcababbce
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-06 20:12:00 -0300

    Styling

--- 20_Laravel/laravel/resources/views/master.blade.php
+++ 20_Laravel/laravel/resources/views/master.blade.php
@@ -7,6 +7,8 @@
 
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     
     <link rel="stylesheet" href="assets/css/pageStyle.css">
 
@@ -20,12 +22,18 @@
         </div>
     </div>
 
-    @yield('content')
-
-    @yield('script')
-
+    @include('partials.topBar')
+    
+    <div id="content">
+        @yield('content')
+    </div>
+    
+    @include('partials.footer')
+        
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
+    @yield('script')
 
     <script src="assets/js/pageScript.js"></script>
 
---
+++ 20_Laravel/laravel/resources/views/partials/topBar.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/"><i class="bi bi-house-fill"></i></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="bi bi-list"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link" href="/login">Login</a>
+          <a class="nav-link" href="/signup">Sign Up</a>
+        </div>
+      </div>
+    </div>
+</nav>
