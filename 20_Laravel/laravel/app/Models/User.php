c56dd0b745a480b5dd60228b28855f59e17da727
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2022-12-08 12:03:59 -0300

    Creating own casts

--- 20_Laravel/laravel/app/Models/User.php
+++ 20_Laravel/laravel/app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         // 'name' => Name::class,
     ];
+
+    // public function getNameAttribute($value)
+    // {
+    //     return 'CFS_'.$value;
+    // }
 }
