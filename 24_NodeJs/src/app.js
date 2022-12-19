78cb5a59e59e32c9bb014a25229027faf8794228
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-19 07:31:55 -0300

    Creating routes

--- 24_NodeJs/src/app.js
+++ 24_NodeJs/src/app.js
@@ -6,6 +6,14 @@ const Home = require('./controllers/Home');
 
 const app = express();
 
+app.get('/user', (request, response) => {
+    return response.json("user");
+});
+
+app.get('/login', (request, response) => {
+    return response.json("login");
+});
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server on");
 })
