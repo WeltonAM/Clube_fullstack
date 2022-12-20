d3996c13bdd8ad5af5a341ace36d8666c2c089f4
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-20 08:53:00 -0300

    Image preview

--- 26_Project3_Resize-Node-Formidable/frontend/src/components/uploads.js
+++ 26_Project3_Resize-Node-Formidable/frontend/src/components/uploads.js
@@ -1,1 +1,30 @@
-console.log('test');
+import "alpinejs";
+
+function upload(){
+    return {
+        choose: function(){
+            const file = document.querySelector("#file");
+            const splitName = file.value.split(".");
+            const extension = splitName[splitName.length - 1];
+            
+            if(!['png','jpeg','jpg'].includes(extension)){
+                console.log("Extension not accepted!");
+                return; 
+            }
+
+            const reader = new FileReader();
+            const imageData = file.files[0];
+
+            reader.onload = (e)=>{
+                const imagePreview = this.$refs.img;
+                imagePreview.width = 350;
+                imagePreview.height = 250
+                imagePreview.src = e.target.result;
+            };
+
+            reader.readAsDataURL(imageData);
+        },
+    };
+}
+
+window.upload = upload;
