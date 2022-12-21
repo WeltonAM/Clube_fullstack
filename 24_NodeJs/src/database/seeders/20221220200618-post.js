16a062b38065f041b33368708f813ac9a940bf64
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-21 08:10:37 -0300

    Organize links

--- 24_NodeJs/src/database/seeders/20221220200618-post.js
+++ 24_NodeJs/src/database/seeders/20221220200618-post.js
@@ -7,7 +7,7 @@ const faker = require("faker");
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
       await queryInterface.bulkInsert("posts", [
         {
           title: faker.lorem.sentence(),
--- 24_NodeJs/src/repository/paginate.js
+++ 24_NodeJs/src/repository/paginate.js
@@ -29,10 +29,21 @@ exports.paginate = {
     render: function(total){
         const totalPages = Math.ceil(total / this.limit);
 
-        let links = `<ul class="pagination">`;
-        for (let index = 1; index <= totalPages; index++) {
-            links += `<li class="page-item"><a href="?page=${index}" class="page-links">${index}</a><li>`;   
+        let links = `<ul class="pagination m-4">`;
+
+        if(this.currentPage > 1){
+            links += `<li class="page-item"><a href="?page=1" class="page-link">First</a><li>`;   
         }
+        
+        for (let index = 1; index <= totalPages; index++) {
+            const active = this.currentPage == index ? 'active' : '';
+            links += `<li class="page-item ${active}"><a href="?page=${index}" class="page-link">${index}</a><li>`;   
+        }
+
+        if(this.currentPage < totalPages){
+            links += `<li class="page-item"><a href="?page=${totalPages}" class="page-link">Last</a><li>`;   
+        }
+        
         links += `</ul>`;
 
         return links;
