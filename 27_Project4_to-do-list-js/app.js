cd77538d1bd53d9553d2626d7bca1e950b4bd3a6
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-21 11:39:28 -0300

    Add task and modal msgs

--- 27_Project4_to-do-list-js/app.js
+++ 27_Project4_to-do-list-js/app.js
@@ -1,5 +1,32 @@
 function todo(){
     return {
-        
+        tasks: [],
+        task: '',
+
+        taskNameExists: function(){
+            return this.tasks.find(task => {
+                return task.name === this.task
+            });
+        },
+
+        addTask: function(){
+            
+            if(this.taskNameExists()){
+                alert('Task already exists');
+                return;
+            }
+            
+            if(this.task === ''){
+                alert('Digit something');
+                return;
+            }
+
+            this.tasks.push({
+                id: Math.floor(Math.random() * new Date().getTime()),
+                name: this.task,
+                completed: false
+            });
+
+            this.task = '';        }
     }
 }
