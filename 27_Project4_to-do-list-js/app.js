2c6ef202f9291ab024ad3507f99763c0273691be
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-21 12:38:54 -0300

    Debugging

--- 27_Project4_to-do-list-js/app.js
+++ 27_Project4_to-do-list-js/app.js
@@ -3,6 +3,11 @@ function todo(){
         tasks: [],
         task: '',
 
+        taskStatus: {
+            completed: 0,
+            uncompleted: 0,
+        },
+
         taskNameExists: function(){
             return this.tasks.find(task => {
                 return task.name === this.task
@@ -28,14 +33,31 @@ function todo(){
             });
 
             this.task = '';
+
+            this.statusTasks();
+        },
+
+        statusTasks: function(){
+            this.taskStatus = this.tasks.reduce((acc, task) => {
+                if(task.completed){
+                    acc['completed']+=1;
+                } else {
+                    acc['uncompleted']+=1;
+                }
+                return acc;
+            }, {completed: 0, uncompleted: 0});
         },
 
         checkTask: function(index){
             this.tasks[index]['completed'] = !this.tasks[index]['completed'];
+
+            this.statusTasks();
         },
         
         removeTask: function(index){
             this.tasks.splice(index, 1);
+
+            this.statusTasks();
         },
     }
 }
