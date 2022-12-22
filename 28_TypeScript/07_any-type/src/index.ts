7636b25fa050a09bdbae88da9913f55bbcc736a2
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-22 09:51:03 -0300

    Any type

---
+++ 28_TypeScript/07_any-type/src/index.ts
@@ -0,0 +1,6 @@
+let data: any[] = [];
+
+data.push("Juliana");
+data.push(27);
+
+console.log(data);
--- 28_TypeScript/public/index.js
+++ 28_TypeScript/public/index.js
@@ -1,17 +1,5 @@
 "use strict";
-function person(name, age) {
-    const str = `My name is ${name} and my age${age}`;
-    console.log(name, age);
-    return str;
-}
-person('Juliana', 27);
-function sum(num1, num2) {
-    console.log(num1 + num2);
-}
-sum(1, 2);
-// --- Overload
-let person1;
-person1 = (name, age) => {
-    return age;
-};
-console.log(person1('Juliana', 27));
+let data = [];
+data.push("Juliana");
+data.push(27);
+console.log(data);
