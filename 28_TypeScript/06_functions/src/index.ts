c0d4b56f18bfe26d1adfb3770aba706c96fb77ee
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-22 09:43:24 -0300

    Overload function

--- 28_TypeScript/06_functions/src/index.ts
+++ 28_TypeScript/06_functions/src/index.ts
@@ -12,3 +12,12 @@ function sum(num1:number, num2:number):void{
 }
 
 sum(1,2);
+
+// --- Overload
+let person1:(a:string, b:number) => number;
+
+person1 = (name:string, age:number) => {
+    return age;
+}
+
+console.log(person1('Juliana', 27));
--- 28_TypeScript/public/index.js
+++ 28_TypeScript/public/index.js
@@ -9,3 +9,9 @@ function sum(num1, num2) {
     console.log(num1 + num2);
 }
 sum(1, 2);
+// --- Overload
+let person1;
+person1 = (name, age) => {
+    return age;
+};
+console.log(person1('Juliana', 27));
