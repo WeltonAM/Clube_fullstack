86918a2ced52d0831dc6606f87a81dfe0deee828
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-22 08:21:09 -0300

    implicit type

---
+++ 28_TypeScript/03_implicit-typing/src/index.ts
@@ -0,0 +1,11 @@
+const myName = 'Juliana';
+let age = 27;
+
+let names = ['Juliana', 'Karla'];
+names.push('Frô');
+
+// names.push(27); ## not assignable 
+
+// age = '27' ## Can not convert type
+
+console.log(age);
