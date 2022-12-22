52c85154d9ec58d4324b8a7cc41b90f010a3856c
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-22 10:08:59 -0300

    Alias

--- 28_TypeScript/08_optional/src/index.ts
+++ 28_TypeScript/08_optional/src/index.ts
@@ -3,3 +3,13 @@ function person(name:string, age:number, isAdmin?:boolean):void{
 }
 
 person('Juliana', 27);
+
+//  --------- Alias 
+
+type NumberOrString = number|string;
+
+function person1(name: NumberOrString, age: NumberOrString, adress: {street: NumberOrString}):void{
+    console.log(name, age, adress);
+}
+
+person1('Juliana', 27, {street:231});
--- 28_TypeScript/public/index.js
+++ 28_TypeScript/public/index.js
@@ -3,3 +3,7 @@ function person(name, age, isAdmin) {
     console.log(name, age, isAdmin);
 }
 person('Juliana', 27);
+function person1(name, age, adress) {
+    console.log(name, age, adress);
+}
+person1('Juliana', 27, { street: 231 });
