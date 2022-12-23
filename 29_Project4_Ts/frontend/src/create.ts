d3567ba37d3d787584c0ed5af0178a723893bf4e
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-23 10:05:26 -0300

    Axios and alpineJs

--- 29_Project4_Ts/frontend/src/create.ts
+++ 29_Project4_Ts/frontend/src/create.ts
@@ -1,1 +1,19 @@
-export const name = 'Juliana';
+import http from "./helpers/http";
+
+function create(){
+    return {
+        create: async function(){
+            try{
+                console.log( await 'ok');
+                
+                const {data} = await http.post('/user/create'); 
+
+                console.log(data);
+            }catch (error){
+                console.log(error)
+            }
+        }
+    }
+}
+
+export default create;
