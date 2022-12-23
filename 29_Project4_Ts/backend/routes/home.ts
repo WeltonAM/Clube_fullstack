edf7802441651aa2ede3d21c9d17004da3e437e8
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-12-23 08:02:07 -0300

    Backend config

---
+++ 29_Project4_Ts/backend/routes/home.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import {index} from '../controllers/Home';
+
+const router = express.Router();
+
+router.get('/', index);
+
+export default router;
---
+++ 29_Project4_Ts/backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+// routes
+import home from '../routes/home';
+
+const app = express();
+
+app.use('/', home);
+
+app.listen(3000, ()=>{
+    console.log('Server on ')
+});
